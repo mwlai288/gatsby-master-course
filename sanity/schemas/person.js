7f0eafbd9cfe021e057379dd2f110878aef5deda
Mateus Lai <mwlai288@gmail.com>
2020-10-06 15:53:32 -0400

    gatsby-course

---
+++ sanity/schemas/person.js
@@ -0,0 +1,44 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+
+export default {
+  // Computer Name
+  name: 'person',
+  // visible title
+  title: 'Slicemasters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+   
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+        description: 'Tell us a bit about this person.'
+      },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+  
+  
+};
